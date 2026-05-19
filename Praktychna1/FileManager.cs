136deec98b82118a73967e0041f8ca0efdc594d1
Vlad Lushchan <vladislav.luschan.2007@outlook.com>
2026-05-19 19:49:42 +0300

    git commit -m "feat: add generic SaveToJson and LoadFromJson methods"

--- Praktychna1/FileManager.cs
+++ Praktychna1/FileManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Praktychna1
@@ -28,6 +29,26 @@ namespace Praktychna1
             {
                 return reader.ReadToEnd();
             }
+        }
+
+        // Додай ці методи всередину класу FileManager:
+
+        public void SaveToJson<T>(T data, string filePath)
+        {
+            // Налаштування для "красивого" вигляду JSON
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(data, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public T LoadFromJson<T>(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("Файл не знайдено");
+
+            string jsonString = File.ReadAllText(filePath);
+            // Десеріалізація об'єкта назад у тип T 
+            return JsonSerializer.Deserialize<T>(jsonString);
         }
     }
 }
--- Praktychna1/obj/Debug/net8.0/Praktychna1.AssemblyInfo.cs
+++ Praktychna1/obj/Debug/net8.0/Praktychna1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Praktychna1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3de007d45fedd82e2b1ec938db576096a1da3b1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b402f258f401d05e239f89424a2b41796681c771")]
 [assembly: System.Reflection.AssemblyProductAttribute("Praktychna1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Praktychna1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
