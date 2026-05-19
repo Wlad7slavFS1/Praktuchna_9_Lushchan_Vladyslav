13b339913fd0e775855b67d21d7fedd792fa4a77
Vlad Lushchan <vladislav.luschan.2007@outlook.com>
2026-05-20 00:19:55 +0300

    git commit -m "docs: complete REPORT8.md and final code alignment with project standards"

--- Praktychna1/obj/Debug/net8.0/Praktychna1.AssemblyInfo.cs
+++ Praktychna1/obj/Debug/net8.0/Praktychna1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Praktychna1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a60f2bfb6e71c127784b1cc21b35dcd736b1967")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f703f74d05a0b65d5139cb72eb498acb91dc0767")]
 [assembly: System.Reflection.AssemblyProductAttribute("Praktychna1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Praktychna1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
