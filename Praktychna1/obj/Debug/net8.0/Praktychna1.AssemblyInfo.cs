0953f044c3bb29f4db8a647f3edc8521177e03c0
Vlad Lushchan <vladislav.luschan.2007@outlook.com>
2026-05-19 20:52:54 +0300

    git commit -m "feat: create custom InvalidFileFormatException class"

--- Praktychna1/obj/Debug/net8.0/Praktychna1.AssemblyInfo.cs
+++ Praktychna1/obj/Debug/net8.0/Praktychna1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Praktychna1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b402f258f401d05e239f89424a2b41796681c771")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+684fcc235ff2e004098fafb06f3a3d5c3690f1eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Praktychna1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Praktychna1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
